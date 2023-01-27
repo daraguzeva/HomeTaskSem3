3cff6adde6bc1f7694c6a7c65bf545990fdada47
Daria <lara-lorn@yandex.ru>
2023-01-27 22:35:04 +0400

    save task

--- task023/Program.cs
+++ task023/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    int A = Convert.ToInt32(Math.Pow(i, 3));
+    Console.WriteLine($"{i}^3 = {A}");
+}
+Console.WriteLine();
+
