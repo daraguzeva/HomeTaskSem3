4c93a85dea6422a101a4200d8155933da9cd965e
Daria <lara-lorn@yandex.ru>
2023-01-27 22:21:05 +0400

    Save task021

--- task021/Program.cs
+++ task021/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите X1: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y1: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z1: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Координаты первой точки: ({X1};{Y1};{Z1})");
+Console.WriteLine();
+Console.Write("Введите X2: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y2: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Координаты первой точки: ({X2};{Y2};{Z2})");
+int D = Convert.ToInt32(Math.Sqrt(Math.Pow((X2-X1),2)+Math.Pow((Y2-Y1),2)+Math.Pow((Z2-Z1),2)));
+Console.WriteLine();
+Console.Write($"Расстояние между точками ({X1};{Y1};{Z1}) и ({X2};{Y2};{Z2}) = {D}");
+
