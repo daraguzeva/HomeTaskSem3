30341b6c44027022965ed9a11977e3af76ec254d
Daria <lara-lorn@yandex.ru>
2023-01-27 21:42:24 +0400

    Save task

--- task019/Program.cs
+++ task019/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
